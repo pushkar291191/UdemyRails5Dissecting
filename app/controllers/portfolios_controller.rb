61ce101e5355f1f46ca64ab6636391a1245f8ed8
Jordan Hudgens <jordan@devcamp.com>
2017-02-16 20:49:11 +0000

    Integrated Edit and Update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,8 +7,12 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.new
   end
   
+  def edit
+    @portfolio_items = Portfolio.find(params[:id])
+  end
+  
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :body))
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
 
     respond_to do |format|
       if @portfolio_items.save
@@ -19,4 +23,19 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def update
+    @portfolio_items = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_items.update(params.require(:portfolio).permit(:title,:subtitle,:body))
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+        
+      else
+        format.html { render :edit }
+        
+      end
+    end
+  end
+
+  
 end
